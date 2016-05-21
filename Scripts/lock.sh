a3f6143b69b6fb73d5ab45c9ed4c1cc58eea7adc
Josh Keating <josh.keating7@gmail.com>
2016-05-21 20:40:51 +0000

    cool lock screen script

---
+++ Scripts/lock.sh
@@ -0,0 +1,8 @@
+# uses i3lock scrot and imagemagick to display a pixelated screen with a lock icon
+# unlocks after password input
+
+scrot /tmp/screen.png
+convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
+convert /home/josh/Pictures/Icons/lock2.png -scale 35% /tmp/lock-icon.png
+convert /tmp/screen.png /tmp/lock-icon.png -gravity center -composite -matte /tmp/screen.png
+i3lock -u -i /tmp/screen.png
