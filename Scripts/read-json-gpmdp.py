b4fb4f7e95f29f5e52320dd24d7ca94b8dab95a1
joshkeating <josh.keating7@gmail.com>
2017-03-17 23:08:45 -0700

    new scrpit for current song

---
+++ Scripts/read-json-gpmdp.py
@@ -0,0 +1,10 @@
+import json
+
+with open('/home/josh/.config/Google Play Music Desktop Player/json_store/playback.json') as data_file:    
+    data = json.load(data_file)
+
+artist_name = data["song"]["artist"]
+
+song_title = data["song"]["title"]
+
+print(artist_name, "-" ,song_title)
