39c6008cb56cbbf930c4d2e80e659978e420e97b
joshkeating <josh.keating7@gmail.com>
2017-03-18 16:10:22 -0700

    will now remove if not playing

--- Scripts/read-json-gpmdp.py
+++ Scripts/read-json-gpmdp.py
@@ -5,8 +5,14 @@ import json
 with open("/home/josh/.config/Google Play Music Desktop Player/json_store/playback.json") as data_file:    
     data = json.load(data_file)
 
-artist_name = data["song"]["artist"]
+is_playing = data["playing"]
 
-song_title = data["song"]["title"]
+if is_playing == "true":
+    artist_name = data["song"]["artist"]
+    song_title = data["song"]["title"]
+    print(artist_name, "-" ,song_title)
 
-print(artist_name, "-" ,song_title)
+else:
+   print("")
+
+
