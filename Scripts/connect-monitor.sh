4f440d022525edaa49900ead9296251d52e0ee29
joshkeating <josh.keating7@gmail.com>
2017-04-13 16:22:20 -0700

    this should be fixed now

--- Scripts/connect-monitor.sh
+++ Scripts/connect-monitor.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Connecting to moniter"
-xrandr --output DP-1 --mode 1920x1080 --pos 0x0 --rotate normal --output eDP-1 --mode 1920x1080 --pos 0x1080 --rotate normal
+xrandr --output VIRTUAL1 --off --output eDP1 --primary --mode 1920x1080 --pos 0x1080 --rotate normal --output DP1 --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI2 --off --output HDMI1 --off --output DP2 --off
 echo "Moniter connected"
