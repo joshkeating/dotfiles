8897e7dee4d212e05d582f5b82895168d3fed3a3
joshkeating <josh.keating7@gmail.com>
2017-09-04 19:18:59 -0400

    minor lock icon improvements

--- Scripts/lock.sh
+++ Scripts/lock.sh
@@ -3,6 +3,6 @@
 
 scrot /tmp/screen.png
 convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
-convert /home/josh/Pictures/icons/lock-fill.png -scale 95% /tmp/lock-icon.png
+convert /home/josh/Pictures/icons/lock-open.png -scale 89% /tmp/lock-icon.png
 convert /tmp/screen.png /tmp/lock-icon.png -gravity center -composite -matte /tmp/screen.png
 i3lock -u -i /tmp/screen.png
