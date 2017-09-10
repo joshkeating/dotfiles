4760f8f000d4efa419428bb7c8fdc20b0c6c16f4
joshkeating <josh.keating7@gmail.com>
2017-09-09 22:21:07 -0400

    lock script should tile on mm correctly

--- Scripts/lock.sh
+++ Scripts/lock.sh
@@ -5,6 +5,6 @@ scrot /tmp/screen.png
 convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
 convert /home/josh/Pictures/icons/lock-open.png -scale 89% /tmp/lock-icon.png
 convert /tmp/screen.png /tmp/lock-icon.png -gravity center -composite -matte /tmp/screen.png
-i3lock -f -i /tmp/screen.png
+i3lock -f -i /tmp/screen.png -t
 # add -u to disable unlock indicator
 
