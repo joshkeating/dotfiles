ecfc21cd2645fdb74ec31ced27662f95cd3c7282
joshkeating <josh.keating7@gmail.com>
2017-09-26 09:13:16 -0700

    color script revamp

--- Scripts/darkbg.sh
+++ Scripts/darkbg.sh
@@ -1,12 +1,33 @@
 #!/bin/bash
 
+# light -> dark
+
 # quick and dirty color switching
 # Colors
 # dark - #1B2428
-# light - #eeeeee
+# light - #EEEEEE
 
-sed -ie 's/*.foreground:   #1B2428/*.foreground:   #eeeeee/g' ~/.Xresources
+sed -ie 's/*.foreground:   #1B2428/*.foreground:   #EEEEEE/g' ~/.Xresources
 sed -ie 's/*.background:   #f8f8f8/*.background:   #1B2428/g' ~/.Xresources
-sed -ie 's/*.cursorColor:  #1B2428/*.cursorColor:  #eeeeee/g' ~/.Xresources
+sed -ie 's/*.cursorColor:  #1B2428/*.cursorColor:  #EEEEEE/g' ~/.Xresources
+
+sed -ie 's/*.color0:       #34454C/*.color0:       #455A64/g' ~/.Xresources
+sed -ie 's/*.color8:       #647C88/*.color8:       #78909C/g' ~/.Xresources
+sed -ie 's/*.color1:       #D32F2F/*.color1:       #B32727/g' ~/.Xresources
+sed -ie 's/*.color9:       #F44336/*.color9:       #F22010/g' ~/.Xresources
+sed -ie 's/*.color2:       #4CAF50/*.color2:       #3F9343/g' ~/.Xresources
+sed -ie 's/*.color10:      #81C784/*.color10:      #65BB69/g' ~/.Xresources
+sed -ie 's/*.color3:       #FBC02D/*.color3:       #FAB406/g' ~/.Xresources
+sed -ie 's/*.color11:      #FFEB3B/*.color11:      #EED500/g' ~/.Xresources
+sed -ie 's/*.color4:       #1976D2/*.color4:       #1562AF/g' ~/.Xresources
+sed -ie 's/*.color12:      #2196F3/*.color12:      #0C82E0/g' ~/.Xresources
+sed -ie 's/*.color5:       #9575CD/*.color5:       #7E58C2/g' ~/.Xresources
+sed -ie 's/*.color13:      #B39DDB/*.color13:      #9C80D0/g' ~/.Xresources
+sed -ie 's/*.color6:       #00897B/*.color6:       #007468/g' ~/.Xresources
+sed -ie 's/*.color14:      #4DB6AC/*.color14:      #409C93/g' ~/.Xresources
+sed -ie 's/*.color7:       #BDBDBD/*.color7:       #A9A9A9/g' ~/.Xresources
+sed -ie 's/*.color15:      #EEEEEE/*.color15:      #DADADA/g' ~/.Xresources
+
+
 
 xrdb ~/.Xresources
--- Scripts/lightbg.sh
+++ Scripts/lightbg.sh
@@ -1,13 +1,32 @@
 #!/bin/bash
 
+# dark -> light
+
 # quick and dirty color switching
 # Colors
 # dark - #1B2428
-# light - #eeeeee
+# light - #EEEEEE
 
-sed -ie 's/*.foreground:   #eeeeee/*.foreground:   #1B2428/g' ~/.Xresources
+sed -ie 's/*.foreground:   #EEEEEE/*.foreground:   #1B2428/g' ~/.Xresources
 sed -ie 's/*.background:   #1B2428/*.background:   #f8f8f8/g' ~/.Xresources
-sed -ie 's/*.cursorColor:  #eeeeee/*.cursorColor:  #1B2428/g' ~/.Xresources
+sed -ie 's/*.cursorColor:  #EEEEEE/*.cursorColor:  #1B2428/g' ~/.Xresources
+
+sed -ie 's/*.color0:       #455A64/*.color0:       #34454C/g' ~/.Xresources
+sed -ie 's/*.color8:       #78909C/*.color8:       #647C88/g' ~/.Xresources
+sed -ie 's/*.color1:       #B32727/*.color1:       #D32F2F/g' ~/.Xresources
+sed -ie 's/*.color9:       #F22010/*.color9:       #F44336/g' ~/.Xresources
+sed -ie 's/*.color2:       #3F9343/*.color2:       #4CAF50/g' ~/.Xresources
+sed -ie 's/*.color10:      #65BB69/*.color10:      #81C784/g' ~/.Xresources
+sed -ie 's/*.color3:       #FAB406/*.color3:       #FBC02D/g' ~/.Xresources
+sed -ie 's/*.color11:      #EED500/*.color11:      #FFEB3B/g' ~/.Xresources
+sed -ie 's/*.color4:       #1562AF/*.color4:       #1976D2/g' ~/.Xresources
+sed -ie 's/*.color12:      #0C82E0/*.color12:      #2196F3/g' ~/.Xresources
+sed -ie 's/*.color5:       #7E58C2/*.color5:       #9575CD/g' ~/.Xresources
+sed -ie 's/*.color13:      #9C80D0/*.color13:      #B39DDB/g' ~/.Xresources
+sed -ie 's/*.color6:       #007468/*.color6:       #00897B/g' ~/.Xresources
+sed -ie 's/*.color14:      #409C93/*.color14:      #4DB6AC/g' ~/.Xresources
+sed -ie 's/*.color7:       #A9A9A9/*.color7:       #BDBDBD/g' ~/.Xresources
+sed -ie 's/*.color15:      #DADADA/*.color15:      #EEEEEE/g' ~/.Xresources
 
 xrdb ~/.Xresources
 
