1589568f574051b565f1728751e24731c702069f
joshkeating <josh.keating7@gmail.com>
2017-10-06 14:09:00 -0700

    light terminal looks a bit better now

--- Scripts/darkbg.sh
+++ Scripts/darkbg.sh
@@ -16,15 +16,15 @@ sed -ie 's/*.color8:       #647C88/*.color8:       #78909C/g' ~/.Xresources
 sed -ie 's/*.color1:       #D32F2F/*.color1:       #B32727/g' ~/.Xresources
 sed -ie 's/*.color9:       #F44336/*.color9:       #F22010/g' ~/.Xresources
 sed -ie 's/*.color2:       #4CAF50/*.color2:       #3F9343/g' ~/.Xresources
-sed -ie 's/*.color10:      #81C784/*.color10:      #65BB69/g' ~/.Xresources
-sed -ie 's/*.color3:       #FBC02D/*.color3:       #FAB406/g' ~/.Xresources
-sed -ie 's/*.color11:      #FFEB3B/*.color11:      #EED500/g' ~/.Xresources
+sed -ie 's/*.color10:      #469e4a/*.color10:      #65BB69/g' ~/.Xresources
+sed -ie 's/*.color3:       #fb8c00/*.color3:       #FAB406/g' ~/.Xresources
+sed -ie 's/*.color11:      #ef6c00/*.color11:      #EED500/g' ~/.Xresources
 sed -ie 's/*.color4:       #1976D2/*.color4:       #1562AF/g' ~/.Xresources
-sed -ie 's/*.color12:      #2196F3/*.color12:      #0C82E0/g' ~/.Xresources
+sed -ie 's/*.color12:      #0c5f9a/*.color12:      #0C82E0/g' ~/.Xresources
 sed -ie 's/*.color5:       #9575CD/*.color5:       #7E58C2/g' ~/.Xresources
-sed -ie 's/*.color13:      #B39DDB/*.color13:      #9C80D0/g' ~/.Xresources
+sed -ie 's/*.color13:      #886cbc/*.color13:      #9C80D0/g' ~/.Xresources
 sed -ie 's/*.color6:       #00897B/*.color6:       #007468/g' ~/.Xresources
-sed -ie 's/*.color14:      #4DB6AC/*.color14:      #409C93/g' ~/.Xresources
+sed -ie 's/*.color14:      #359188/*.color14:      #409C93/g' ~/.Xresources
 sed -ie 's/*.color7:       #BDBDBD/*.color7:       #A9A9A9/g' ~/.Xresources
 sed -ie 's/*.color15:      #EEEEEE/*.color15:      #DADADA/g' ~/.Xresources
 
--- Scripts/lightbg.sh
+++ Scripts/lightbg.sh
@@ -16,15 +16,15 @@ sed -ie 's/*.color8:       #78909C/*.color8:       #647C88/g' ~/.Xresources
 sed -ie 's/*.color1:       #B32727/*.color1:       #D32F2F/g' ~/.Xresources
 sed -ie 's/*.color9:       #F22010/*.color9:       #F44336/g' ~/.Xresources
 sed -ie 's/*.color2:       #3F9343/*.color2:       #4CAF50/g' ~/.Xresources
-sed -ie 's/*.color10:      #65BB69/*.color10:      #81C784/g' ~/.Xresources
-sed -ie 's/*.color3:       #FAB406/*.color3:       #FBC02D/g' ~/.Xresources
-sed -ie 's/*.color11:      #EED500/*.color11:      #FFEB3B/g' ~/.Xresources
+sed -ie 's/*.color10:      #65BB69/*.color10:      #469e4a/g' ~/.Xresources
+sed -ie 's/*.color3:       #FAB406/*.color3:       #fb8c00/g' ~/.Xresources
+sed -ie 's/*.color11:      #EED500/*.color11:      #ef6c00/g' ~/.Xresources
 sed -ie 's/*.color4:       #1562AF/*.color4:       #1976D2/g' ~/.Xresources
-sed -ie 's/*.color12:      #0C82E0/*.color12:      #2196F3/g' ~/.Xresources
+sed -ie 's/*.color12:      #0C82E0/*.color12:      #0c5f9a/g' ~/.Xresources
 sed -ie 's/*.color5:       #7E58C2/*.color5:       #9575CD/g' ~/.Xresources
-sed -ie 's/*.color13:      #9C80D0/*.color13:      #B39DDB/g' ~/.Xresources
+sed -ie 's/*.color13:      #9C80D0/*.color13:      #886cbc/g' ~/.Xresources
 sed -ie 's/*.color6:       #007468/*.color6:       #00897B/g' ~/.Xresources
-sed -ie 's/*.color14:      #409C93/*.color14:      #4DB6AC/g' ~/.Xresources
+sed -ie 's/*.color14:      #409C93/*.color14:      #359188/g' ~/.Xresources
 sed -ie 's/*.color7:       #A9A9A9/*.color7:       #BDBDBD/g' ~/.Xresources
 sed -ie 's/*.color15:      #DADADA/*.color15:      #EEEEEE/g' ~/.Xresources
 
