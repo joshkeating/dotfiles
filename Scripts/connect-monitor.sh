87aff36b0a9cf9e22b49a174728b52b1dc64e04d
joshkeating <josh.keating7@gmail.com>
2017-10-21 03:55:33 -0700

    brightness change, fucking fractional scaling...

--- Scripts/connect-monitor.sh
+++ Scripts/connect-monitor.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Connecting to moniter"
-xrandr --output VIRTUAL1 --off --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --mode 1920x1080 --pos 1920x0 --rotate normal
+xrandr --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --auto --scale 1x1 --right-of eDP1
 
 echo "Moniter connected"
