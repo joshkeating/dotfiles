41733fdf89890c447c50762562cc1c6101ce8f34
joshkeating <josh.keating7@gmail.com>
2017-10-23 12:17:05 -0700

    display scaling got me again

--- Scripts/connect-monitor.sh
+++ Scripts/connect-monitor.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Connecting to moniter"
-xrandr --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --auto --scale 1x1 --right-of eDP1
+xrandr --output VIRTUAL1 --off --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --mode 1920x1080 --pos 1920x0 --rotate normal
 
 echo "Moniter connected"
