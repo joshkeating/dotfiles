74d62b911cae841756d275975ea4a25a5089a258
joshkeating <josh.keating7@gmail.com>
2018-10-15 00:31:40 -0700

    more wall

--- Scripts/disconnect-monitor.sh
+++ Scripts/disconnect-monitor.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Removing second monitor"
 xrandr --output DP1 --off
-feh --bg-scale /home/josh/Pictures/walls/trees-and-mist/DeskA.png
+feh --bg-scale /home/josh/Pictures/walls/macos-walls/Abstract\ 9.jpg
 echo "Moniter removed"
 # restart polybar in place
 nohup $HOME/.config/polybar/launch.sh &>/dev/null &
