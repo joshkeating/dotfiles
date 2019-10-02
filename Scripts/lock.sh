89854ebd8b2823f06cb216ba7ed3ebdc4398371c
joshkeating <josh.keating7@gmail.com>
2019-10-01 21:54:43 -0700

    switch to old pixelated screenlocker

--- Scripts/lock.sh
+++ Scripts/lock.sh
@@ -1,16 +1,11 @@
 # uses i3lock, scrot, and imagemagick to display a pixelated screen with a lock icon
 # unlocks after password input
 
-# scrot /tmp/screen.png
-# convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
+scrot /tmp/screen.png
+convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
 
 # uncomment for lock icon
-# convert /home/josh/Pictures/icons/lock-open.png -scale 89% /tmp/lock-icon.png
+# convert ~/Pictures/icons/lock-open.png -scale 89% /tmp/lock-icon.png
 # convert /tmp/screen.png /tmp/lock-icon.png -gravity center -composite -matte /tmp/screen.png
 
-# i3lock -f -i /tmp/screen.png -t
-
-# add -u to disable unlock indicator
-
-# lock with image
-i3lock -f -i ~/Pictures/lockscreen/lockscreennocirclepng.png -t
+i3lock -f -i /tmp/screen.png -t
